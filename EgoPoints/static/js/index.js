488fccbbad436f80d62fa1a91946e3e307e2204d
AhmadDarKhalil <ahmad.darkhali@gmail.com>
2024-12-09 19:26:45 +0000

    update

--- EgoPoints/static/js/index.js
+++ EgoPoints/static/js/index.js
@@ -84,3 +84,68 @@ $(document).ready(function () {
         });
     });
 });
+
+
+$(document).ready(function () {
+    // Carousel options
+    var options = {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        loop: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 13000,
+    };
+
+    // Initialize all carousels
+    var carousels = bulmaCarousel.attach('.carousel3', options);
+	
+
+    // Attach slider
+    bulmaSlider.attach();
+
+    // Reset video when the slide changes
+    carousels.forEach(carousel => {
+        carousel.on('before:show', state => {
+            // Reset all videos
+            carousel.slides.forEach(slide => {
+                const video = slide.querySelector('video');
+                if (video) {
+                    // video.pause();          // Pause the video
+                    video.currentTime = 0; // Reset to the start
+                }
+            });
+        });
+
+        carousel.on('after:show', state => {
+            // Optionally autoplay video if the current slide has a video
+            const currentSlide = carousel.slides[state.index];
+            const video = currentSlide.querySelector('video');
+            if (video) {
+                video.play();
+            }
+        });
+    });
+});
+
+
+$(document).ready(function () {
+    // Carousel options
+    var options = {
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        loop: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    };
+
+    // Initialize all carousels
+    var carousels = bulmaCarousel.attach('.carousel4', options);
+	
+
+    // Attach slider
+    bulmaSlider.attach();
+
+});
+
